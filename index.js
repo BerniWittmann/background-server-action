c78eb6a1e3d32d8f9b71415492fc63bb921513b7
Bernhard Wittmann <bernhard@monkee.rocks>
2021-07-26 08:34:07 +0200

    feat: :sparkles: add initial implementation

--- index.js
+++ index.js
@@ -1,21 +1,195 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const exec = require('@actions/exec');
+const io = require('@actions/io');
+const os = require('os')
+const debug = require('debug')('background-server-action');
+const cliParser = require('argument-vector')();
+const quote = require('quote');
+
+const { ping } = require('./src/ping')
+
+const isWindows = () => os.platform() === 'win32'
+const isUrl = (s) => /^https?:\/\//.test(s)
+
+
+/**
+ * Parses input command, finds the tool and
+ * the runs the command.
+ */
+ const execCommand = (
+  fullCommand,
+  waitToFinish = true,
+  label = 'executing'
+) => {
+  console.log('%s command "%s"', label, fullCommand)
+
+  const args = cliParser.parse(fullCommand)
+  debug(`parsed command: ${args.join(' ')}`)
+
+  return io.which(args[0], true).then((toolPath) => {
+    debug(`found command "${toolPath}"`)
+    debug(`with arguments ${args.slice(1).join(' ')}`)
+
+    const toolArguments = args.slice(1)
+    const argsString = toolArguments.join(' ')
+    debug(`running ${quote(toolPath)} ${argsString}`)
+    debug(`waiting for the command to finish? ${waitToFinish}`)
+
+    const promise = exec.exec(
+      quote(toolPath),
+      toolArguments,
+      {}
+    )
+    if (waitToFinish) {
+      return promise
+    }
+  })
+}
 
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+  let command
+  if (isWindows()) {
+    // allow custom Windows command command
+    command =
+      core.getInput('command-windows') || core.getInput('command')
+  } else {
+    command = core.getInput('command')
   }
+  if (!command) {
+    return
+  }
+  // allow commands to be separated using commas or newlines
+  const separateCommands = command
+    .split(/,|\n/)
+    .map((s) => s.trim())
+    .filter(Boolean)
+  debug(
+  `Separated ${
+    separateCommands.length
+  } main commands ${separateCommands.join(', ')}`
+  )
+
+  return separateCommands.map((command) => {
+    return execCommand(
+      command,
+      false,
+      `run command "${command}`
+    )
+  })
 }
 
-run();
+const buildAppMaybe = () => {
+  const buildApp = core.getInput('build')
+  if (!buildApp) {
+    return
+  }
+
+  debug(`building application using "${buildApp}"`)
+
+  return execCommand(buildApp, true, 'build app')
+}
+
+const startServersMaybe = () => {
+  let startCommand
+
+  if (isWindows()) {
+    // allow custom Windows start command
+    startCommand =
+      core.getInput('start-windows') || core.getInput('start')
+  } else {
+    startCommand = core.getInput('start')
+  }
+  if (!startCommand) {
+    debug('No start command found')
+    return Promise.resolve()
+  }
+
+  // allow commands to be separated using commas or newlines
+  const separateStartCommands = startCommand
+    .split(/,|\n/)
+    .map((s) => s.trim())
+    .filter(Boolean)
+  debug(
+    `Separated ${
+      separateStartCommands.length
+    } start commands ${separateStartCommands.join(', ')}`
+  )
+
+  return separateStartCommands.map((startCommand) => {
+    return execCommand(
+      startCommand,
+      false,
+      `start server "${startCommand}`
+    )
+  })
+}
+
+/**
+ * Pings give URL(s) until the timeout expires.
+ * @param {string} waitOn A single URL or comma-separated URLs
+ * @param {Number?} waitOnTimeout in seconds
+ */
+ const waitOnUrl = (waitOn, waitOnTimeout = 60) => {
+  console.log(
+    'waiting on "%s" with timeout of %s seconds',
+    waitOn,
+    waitOnTimeout
+  )
+
+  const waitTimeoutMs = waitOnTimeout * 1000
+
+  const waitUrls = waitOn
+    .split(',')
+    .map((s) => s.trim())
+    .filter(Boolean)
+  debug(`Waiting for urls ${waitUrls.join(', ')}`)
+
+  // run every wait promise after the previous has finished
+  // to avoid "noise" of debug messages
+  return waitUrls.reduce((prevPromise, url) => {
+    return prevPromise.then(() => {
+      debug(`Waiting for url ${url}`)
+      return ping(url, waitTimeoutMs)
+    })
+  }, Promise.resolve())
+}
+
+const waitOnMaybe = () => {
+  const waitOn = core.getInput('wait-on')
+  if (!waitOn) {
+    return
+  }
+
+  const waitOnTimeout = core.getInput('wait-on-timeout') || '60'
+  const timeoutSeconds = parseFloat(waitOnTimeout)
+
+  if (isUrl(waitOn)) {
+    return waitOnUrl(waitOn, timeoutSeconds)
+  }
+
+  console.log('Waiting using command "%s"', waitOn)
+  return execCommand(waitOn, true)
+}
+
+buildAppMaybe()
+  .then(startServersMaybe)
+  .then(waitOnMaybe)
+  .then(run)
+  .then(() => {
+    debug('all done, exiting')
+    // force exit to avoid waiting for child processes,
+    // like the server we have started
+    // see https://github.com/actions/toolkit/issues/216
+    process.exit(0)
+  })
+  .catch((error) => {
+    // final catch - when anything goes wrong, throw an error
+    // and exit the action with non-zero code
+    debug(error.message)
+    debug(error.stack)
+
+    core.setFailed(error.message)
+    process.exit(1)
+  })
---
+++ src/ping.js
@@ -0,0 +1,81 @@
+// eslint-disable-next-line no-unused-vars
+const core = require('@actions/core')
+const got = require('got')
+const debug = require('debug')('@cypress/github-action')
+
+/**
+ * A small utility for checking when an URL responds, kind of
+ * a poor man's https://www.npmjs.com/package/wait-on. This version
+ * is implemented using https://github.com/sindresorhus/got
+ */
+const ping = (url, timeout) => {
+  if (!timeout) {
+    throw new Error('Expected timeout in ms')
+  }
+
+  // make copy of the error codes that "got" retries on
+  const errorCodes = [...got.defaults.options.retry.errorCodes]
+  errorCodes.push('ESOCKETTIMEDOUT')
+
+  // we expect the server to respond within a time limit
+  // and if it does not - retry up to total "timeout" duration
+  const individualPingTimeout = Math.min(timeout, 30000)
+
+  // add to the timeout max individual ping timeout
+  // to avoid long-waiting ping from "rolling" over the end
+  // and preventing pinging the last time
+  timeout += individualPingTimeout
+  const limit = Math.ceil(timeout / individualPingTimeout)
+
+  debug(`total ping timeout ${timeout}`)
+  debug(`individual ping timeout ${individualPingTimeout}ms`)
+  debug(`retries limit ${limit}`)
+
+  const start = +new Date()
+  return got(url, {
+    headers: {
+      Accept: 'text/html, application/json, text/plain, */*'
+    },
+    timeout: individualPingTimeout,
+    errorCodes,
+    retry: {
+      limit,
+      calculateDelay({ error, attemptCount }) {
+        if (error) {
+          debug(`got error ${JSON.stringify(error)}`)
+        }
+        const now = +new Date()
+        const elapsed = now - start
+        debug(
+          `${elapsed}ms ${error.method} ${error.host} ${error.code} attempt ${attemptCount}`
+        )
+        if (elapsed > timeout) {
+          console.error(
+            '%s timed out on retry %d of %d, elapsed %dms, limit %dms',
+            url,
+            attemptCount,
+            limit,
+            elapsed,
+            timeout
+          )
+          return 0
+        }
+
+        // if the error code is ECONNREFUSED use shorter timeout
+        // because the server is probably starting
+        if (error.code === 'ECONNREFUSED') {
+          return 1000
+        }
+
+        // default "long" timeout
+        return individualPingTimeout
+      }
+    }
+  }).then(() => {
+    const now = +new Date()
+    const elapsed = now - start
+    debug(`pinging ${url} has finished ok after ${elapsed}ms`)
+  })
+}
+
+module.exports = { ping }
